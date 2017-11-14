608e4590e7a0ace0872c89e30b92b1fb7977191a
Alejandro Nava Luna <31482619+iconava@users.noreply.github.com>
2017-11-13 18:20:59 -0600

    Tarea 21.

---
+++ Tarea 21/letras.h
@@ -0,0 +1,31 @@
+void letraa(){
+	printf("A A A A\nA     A\nA  A  A\nA     A\nA     A\n\n");
+}
+
+void letral(){
+	printf("L\nL\nL\nL\nL L L L\n\n");
+}
+
+void letrae(){
+	printf("E E E E\nE\nE E E\nE\nE E E E\n\n");
+}
+
+void letraj(){
+	printf("J J J J\n    J\n    J\n    J\nJ J J\n\n");
+}
+
+void letran(){
+	printf("N     N\nN N   N\nN   N N\nN     N\nN     N\n\n");
+}
+
+void letrad(){
+	printf("D D D\nD     D\nD     D\nD     D\nD D D\n\n");
+}
+
+void letrar(){
+	printf("R R R R\nR     R\nR R R R\nR  R\nR     R\n\n");
+}
+
+void letrao(){
+	printf("O O O O\nO     O\nO     O\nO     O\nO O O O\n\n");
+}
---
+++ Tarea 21/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cabecera.h"
+#include "letras.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	appInfoData("Tarea 21. Letras","13/11/2017");
+	
+	letraa();
+	letral();
+	letrae();
+	letraj();
+	letraa();
+	letran();
+	letrad();
+	letrar();
+	letrao();
+	
+	system ("pause");
+	return 0;
+}
