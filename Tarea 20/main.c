cecbad676b5539ddd60d07c21d26611f8b51cb48
Alejandro Nava Luna <31482619+iconava@users.noreply.github.com>
2017-11-13 18:17:20 -0600

    Tarea 20
    Imprime el nombre con letras hechas por letras, valga la redundancia.

--- Tarea 20/main.c
+++ Tarea 20/main.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "tarea20.h"
+#include "cabecera.h"
+#include "letras.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	//Cuadrado
-	int r=0;
-	int b=0;
-	int h=0;
-	printf("Ingrese altura del cuadrado:\n");
-	scanf("%d",&r);
-	printf("ingrese el ancho del cuadrado:\n");
-	scanf("%d",&b);
-	h=fnCuadrado(b,r);	
 	
-		//Iva
-	float c=0.0;
-	float res;
-	printf("Teclea una cantidad\n");
-	scanf("%f",&c);
-	res=fnIva(c);
-	printf("\n",res);
+	appInfoData("Tarea 21. Letras","13/11/2017");
+	
+	letraa();
+	letral();
+	letrae();
+	letraj();
+	letraa();
+	letran();
+	letrad();
+	letrar();
+	letrao();
 	
 	system ("pause");
-	
 	return 0;
 }
